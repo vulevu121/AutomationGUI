3a9d5ed56fd7ff3c96bb10f631dc5d271261d454
Vu Le <vulevu121@gmail.com>
2018-10-24 15:49:11 -0700

    Added command line setup

---
+++ setup.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\Python27\Python.exe" -m pip install --upgrade pip
+"C:\Program Files\Python27\Python.exe" -m pip install python-qt5
+"C:\Program Files\Python27\Python.exe" -m pip install xmltodict
+"C:\Program Files\Python27\Python.exe" -m pip install pathlib
+
+pause
